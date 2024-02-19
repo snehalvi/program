ecc6373f09390945db3290b27e5064d26ca06158
snehal1951 <snehalingole1951@gmail.com>
2024-02-19 11:58:54 +0530

    Added new files

---
+++ addition/add.java
@@ -0,0 +1,14 @@
+package addition;
+import java.util.Scanner;
+
+public class add {
+    public static void main(String[] args) {
+        int x, y;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the values of x & y:");
+        x = sc.nextInt();
+        y = sc.nextInt();
+        int sum = x + y;
+        System.out.println(sum);
+    }
+}
